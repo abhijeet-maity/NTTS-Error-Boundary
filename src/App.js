e6d8e9ea9b41d81ad03b4a001599fdda521be84a
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-02-18 11:57:23 +0530

    added Error Boundary at individual routing level

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
-import Dashboard from './Pages/Dashboard';
-import ErrorBoundary from './Components/ErrorBoundary';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Dashboard from "./Pages/Dashboard";
+import ErrorBoundary from "./Components/ErrorBoundary";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <ErrorBoundary>
-        <Routes>
-          <Route to="/" element={<Home/>}/>
-          <Route to="/about" element={<About/>}/>
-          <Route to="/contact" element={<Contact/>}/>
-          <Route to="/dashboard" element={<Dashboard/>}/>
-        </Routes>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/dashboard" element={
+              <ErrorBoundary>
+                <Dashboard />
+              </ErrorBoundary>} />
+          </Routes>
         </ErrorBoundary>
       </Router>
     </div>
